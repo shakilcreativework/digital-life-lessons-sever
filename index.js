801f2abc9611d5bc025454d39eb18812e99fca32
Shakil Ahmed <shakil.devcraft@gmail.com>
2026-07-01 00:07:56 +0600

    Now, add new codes

--- index.js
+++ index.js
@@ -13,9 +13,12 @@ const port = process.env.PORT || 5000;
 // Middleware
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:3000",
-    credentials: true,
+    origin: [
+        "http://localhost:3000", // For local development
+        // "https://digital-life-lessons-client-one.vercel.app" // YOUR ACTUAL DEPLOYED VERCEL URL
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
   }),
 );
 app.use(express.json());
@@ -32,7 +35,7 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    await client.connect();
+    // await client.connect();
 
     const db = client.db("digitalLifeLessionsDB");
     const lessonsCollection = db.collection("lessons");
@@ -904,7 +907,7 @@ async function run() {
     // ==========================================
     // DATABASE CHECK / MONITORING
     // ==========================================
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
